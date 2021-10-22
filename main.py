ab5d6a8e9df209a1e2a1db66ca6d3069374b2fa3
Salim Al Kharsa <salimalkharsa@gmail.com>
2021-10-22 11:30:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,45 @@
+#Imports
+import pandas as pd
+import datetime
+import numpy as np
+from nba_api.stats.endpoints import shotchartdetail
+from nba_api.stats.static import teams
+from nba_api.stats.static import players
+import requests
+import json
+import time
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.model_selection import cross_val_score
+import statistics as s
+import math as m
+
+#Import of Other Files
+from Scrape-CleanShotData.py import CleanData as SCD
+
+def GetData():
+    #Loading basic info by getting user input
+    print("This is the shot scraping and analysis program, follow the instructions below to go through the program...")
+    DataType = input("  Input 1 if you want a team's data; 2 if you want a player's data (If you screw this step up, the program will crash so enter a 1 or a 2...): ")
+    #Team info
+    nba_teams = teams.get_teams()
+    nba_players = players.get_players()
+    TeamID = 0
+    PlayerID = 0
+    if(DataType == 1):
+        while(True):
+            try:
+                TeamAbb = input("       Input the team abbreviation you need data for, if you want all team's data enter 0! ")
+                if(TeamAbb=='0'):
+                    TeamID = 0
+                else:
+                    Team = [team for team in nba_teams if team['abbreviation'] == TeamAbb][0]
+                False
+            except:
+                print("Try again, input could not be read...")
+    if(DataType == 2):
+        while(True):
+            try:
                 PlayerIn = input("       Input the player full name you need data for, if you want all player's data enter 0! ")
                 if(PlayerIn=='0'):
                     PlayerID = 0
